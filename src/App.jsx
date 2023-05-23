e7ae16d3407a3cfb0f55322d4f895e51559db098
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-05-23 01:06:27 -0300

    se realiza maquetado y logica del ejercicio 3, se comprueba el funcionamiento del mismo, operando correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import HelloWorld from './components/HelloWorld'
 
 function App() {
 
   return (
     <>
-      
+      <HelloWorld textoAgregado="My Friend" signoAdmiracion="!"></HelloWorld>
     </>
   )
 }
