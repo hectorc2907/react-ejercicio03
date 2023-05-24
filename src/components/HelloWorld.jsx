3415229f6d39e6b726683e9780fbe9b0c253b663
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-05-23 22:17:04 -0300

    se corrige el nombre de la pestaña y la logica del componente HelloWorld para que pueda funcionar con un solo boton

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 const HelloWorld = (props) => {
     
 const [texto,setTexto] = useState(false);
-const mostrarTexto = () => {
-    setTexto(true);
+const ejecutarAccionMostrarOcultar = () => {
+    setTexto(!texto);
 };
     
     return (
         <>
             <h1>Hello {props.textoAgregado} {texto && "(from changed state)"}{props.signoAdmiracion}</h1>
-            <button onClick={mostrarTexto}>Click me</button>
+            <button onClick={ejecutarAccionMostrarOcultar}>Click Me!</button>
         </>
     );
 };
